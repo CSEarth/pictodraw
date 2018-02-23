a9e23d9608a6b475ef518153534af4cf707682e8
FrankJuH <frank@preferredreturn.com>
2018-02-23 12:30:53 -0500

    Initial Changes - to fix merge conflicts

--- client/redux/actions/actions.js
+++ client/redux/actions/actions.js
@@ -1,25 +1,25 @@
 import * as types from './actionTypes'
 
-const setDrawer = () => {
+const setDrawer = (name) => {
 	return {
 		type: types.SET_DRAWER
 	}
 }
 
-const addMessage = () => {
+const addMessage = (newMessage) => {
 	return {
 	type: types.ADD_MESSAGE
 }
 
 
-const addUser = () => {
+const addUser = (oldName,newName) => {
 	return {
 		type: types.ADD_USER
 	}
 }
 
 
-const guessInput = () => {
+const guessInput = (input) => {
 	return {
 		type: types.GUESS_INPUT
 	}
--- client/redux/store.js
+++ client/redux/store.js
@@ -1,11 +1,8 @@
 import { createStore, applyMiddleware} from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducers/mainReducer';
 
-// we are adding composeWithDevTools here to get easy access to the Redux dev tools
 const store = createStore(
-  reducer,
-  composeWithDevTools()
+  reducer
 );
 
 export default store;
